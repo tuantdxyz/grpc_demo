c73029de6240a7e574c11c2b09dbfb6d350ba7de
TuanTD <tuantd16@fpt.com.vn>
2021-09-15 17:17:23 +0700

    grpc ruby demo first commit

--- examples/ruby/employee_server.rb
+++ examples/ruby/employee_server.rb
@@ -23,22 +23,29 @@ lib_dir = File.join(this_dir, 'lib')
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
 require 'grpc'
-require 'helloworld_services_pb'
+require 'employee_services_pb'
 
 # GreeterServer is simple server that implements the Helloworld Greeter server.
-class GreeterServer < Helloworld::Greeter::Service
-  # say_hello implements the SayHello rpc method.
-  def say_hello(hello_req, _unused_call)
-    Helloworld::HelloReply.new(message: "Hello #{hello_req.name}")
+class EmployeeServer < Helloworld::Employee::Service
+  def get_employee(req, _unused_call)
+    puts(req.id)
+    #TODO handle data with client, CRUD database
+    # response result to client
+    Helloworld::EmployeeRes.new(employeeId: req.id)
   end
 end
-
+# class GreeterServer < Helloworld::Greeter::Service
+#   # say_hello implements the SayHello rpc method.
+#   def say_hello(hello_req, _unused_call)
+#     Helloworld::HelloReply.new(message: "Hello #{hello_req.name}")
+#   end
+# end
 # main starts an RpcServer that receives requests to GreeterServer at the sample
 # server port.
 def main
   s = GRPC::RpcServer.new
-  s.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
-  s.handle(GreeterServer)
+  s.add_http2_port('0.0.0.0:50052', :this_port_is_insecure)
+  s.handle(EmployeeServer)
   # Runs the server with SIGHUP, SIGINT and SIGQUIT signal handlers to 
   #   gracefully shutdown.
   # User could also choose to run server via call to run_till_terminated
